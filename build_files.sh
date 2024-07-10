28e1ceb892db0bb8073a6a17c64157cc485a3486
LAPTOP-CEKV0UP6\edwin <edwinwen75@gmail.com>
2024-07-10 16:17:22 -0400

    fixing build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate 
+python3 manage.py makemigrations
+python3 manage.py migrate 
 
-python3.9 manage.py collectstatic
+python3 manage.py collectstatic
