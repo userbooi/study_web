95536f34f02710f5e4f519b4fdb8a191f85a7f37
Edwin Wen <edwinwen75@gmail.com>
2024-07-22 12:23:28 -0400

    debug = false

--- study_web/settings.py
+++ study_web/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
